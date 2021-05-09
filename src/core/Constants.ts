18f86c9277f6d005d36e3c42337fead7392aacef
dbenitez-bcn <dbr.bcn@gmail.com>
2021-05-09 14:45:21 +0200

    adds stage as default

--- src/core/Constants.ts
+++ src/core/Constants.ts
@@ -2,5 +2,5 @@ export default class Constants {
     static storage = {
         TOKEN_KEY: 'token_key'
     }
-    static BASE_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:9000' : 'https://dumbbell-backend.herokuapp.com';
+    static BASE_URL = process.env.NODE_ENV !== 'production' ? 'https://dumbbell-backend-stage.herokuapp.com' : 'https://dumbbell-backend.herokuapp.com';
 }
