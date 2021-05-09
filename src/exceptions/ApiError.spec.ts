39f7f5e9264fdca86bb3b7dd0192b83115dd84d8
dbenitez-bcn <dbr.bcn@gmail.com>
2021-05-09 03:38:39 +0200

    fix test

--- src/exceptions/ApiError.spec.ts
+++ src/exceptions/ApiError.spec.ts
@@ -2,7 +2,7 @@ import ApiError from "./ApiError";
 
 describe('ApiError', () => {
     it("Should return the correct string when toString is called", ()=> {
-        const actual = new ApiError(100, {message: "Error message"});
+        const actual = new ApiError(100, "Error message");
         const expected = "100 - Error message";
         expect(actual.toString()).toBe(expected)
     });
