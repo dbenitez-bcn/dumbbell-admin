8084ef63c1f5b6ab297f48df567b5f7de5e9fb3d
dbenitez-bcn <dbr.bcn@gmail.com>
2021-05-25 20:58:15 +0200

    adds navigation bar

---
+++ components/navigation/LogoutButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-list-item link color="error" input-value="true" @click="logout">
+    <v-list-item-icon>
+      <v-icon color="error">mdi-power</v-icon>
+    </v-list-item-icon>
+    <v-list-item-title>Logout</v-list-item-title>
+  </v-list-item>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { getModule } from "vuex-module-decorators";
+import SessionStore from "~/store/session";
+
+@Component
+export default class NavigationBar extends Vue {
+  logout() {
+    getModule(SessionStore, this.$store).logout();
+    this.$router.push("/access");
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ components/navigation/NavigationBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-navigation-drawer permanent expand-on-hover app>
+    <v-list nav dense>
+      <v-list-item-group color="primary-dark">
+        <v-list-item
+          v-for="(item, i) in navButtons"
+          :key="i"
+          nuxt
+          :to="item.to"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-title v-text="item.title"></v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+    <template v-slot:append>
+      <v-divider></v-divider>
+      <logout-button />
+    </template>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import LogoutButton from "./LogoutButton.vue";
+
+@Component({
+  components: {
+LogoutButton
+  }
+})
+export default class NavigationBar extends Vue {
+  private navButtons = [
+    {
+      icon: "mdi-dumbbell",
+      to: "exercises",
+      title: "Exercises",
+    },
+    {
+      icon: "mdi-toggle-switch",
+      to: "toggles",
+      title: "Toggles",
+    },
+  ];
+}
+</script>
+
+<style>
+</style>
