64a30cacbd075e3c1d648cfb76d55316146c9b06
dbenitez-bcn <dbr.bcn@gmail.com>
2021-05-30 00:22:56 +0200

    removes token key constant

--- src/core/Constants.ts
+++ src/core/Constants.ts
@@ -1,6 +1,3 @@
 export default class Constants {
-    static storage = {
-        TOKEN_KEY: 'token_key'
-    }
     static BASE_URL = process.env.NODE_ENV !== 'production' ? 'https://dumbbell-backend-stage.herokuapp.com' : 'https://dumbbell-backend.herokuapp.com';
 }
