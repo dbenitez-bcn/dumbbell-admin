f6dbc9a7fd8712e0f1238f2695741bdefd612014
dbenitez-bcn <dbr.bcn@gmail.com>
2021-05-29 20:44:20 +0200

    creates exercise store

---
+++ store/exercise.spec.ts
@@ -0,0 +1,45 @@
+import Vuex from 'vuex';
+import { createLocalVue } from "@vue/test-utils";
+import { container } from "~/src/core/Container";
+import { SYMBOLS } from "~/src/core/SYMBOLS";
+import IExerciseRepository from "~/src/repositories/types/IExerciseRepository";
+import { getModule } from 'vuex-module-decorators';
+import ExerciseStore from './exercise';
+import { FakeExerciseRepository } from '~/src/repositories/__mocks__/FakeExerciseRepository';
+import Exercise from '~/src/models/domain/Exercise';
+import ExerciseVM from '~/src/models/viewModels/ExerciseVM';
+
+describe("Exercise store", () => {
+    let sut: ExerciseStore;
+    let repository: FakeExerciseRepository;
+
+    beforeAll(() => {
+        container
+            .bind<IExerciseRepository>(SYMBOLS.ExerciseRepository)
+            .to(FakeExerciseRepository)
+            .inSingletonScope();
+        repository = container
+            .get<IExerciseRepository>(SYMBOLS.ExerciseRepository) as FakeExerciseRepository;
+        const Vue = createLocalVue();
+        Vue.use(Vuex)
+    })
+
+    beforeEach(() => {
+        const store = new Vuex.Store({
+            modules: {
+                exercise: ExerciseStore
+            }
+        })
+        sut = getModule(ExerciseStore, store);
+    })
+
+    it("Should fetch all exercises", async () => {
+        repository.getAll.mockResolvedValue([new Exercise(1, "name", "description", 8)]);
+        await sut.fetchExercises();
+
+        const got = sut.exercises;
+
+        expect(got).toStrictEqual([new ExerciseVM(1, "name", "description", 8)]);
+        expect(repository.getAll).toBeCalled();
+    })
+})
---
+++ store/exercise.ts
@@ -0,0 +1,32 @@
+import { Module, VuexModule, Action, Mutation } from "vuex-module-decorators";
+import { lazyInject } from "~/src/core/Container";
+import { SYMBOLS } from "~/src/core/SYMBOLS";
+import Exercise from "~/src/models/domain/Exercise";
+import ExerciseVM from "~/src/models/viewModels/ExerciseVM";
+import IExerciseRepository from "~/src/repositories/types/IExerciseRepository";
+
+@Module({
+    name: 'exercise',
+    namespaced: true,
+    stateFactory: true
+})
+export default class ExerciseStore extends VuexModule {
+
+    @lazyInject(SYMBOLS.ExerciseRepository) repository!: IExerciseRepository
+
+    private list: Exercise[] = [];
+
+    @Mutation
+    private setExercises(exercises: Exercise[]) {
+        this.list = exercises;
+    }
+
+    @Action({ rawError: true })
+    async fetchExercises() {
+        this.setExercises(await this.repository.getAll());
+    }
+
+    get exercises(): ExerciseVM[] {
+        return this.list.map(exercise => exercise.toVM());
+    }
+}
