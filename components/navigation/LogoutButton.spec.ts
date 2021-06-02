e85e7f2a66fc96f3ad71abacbcb9b799626c8148
dbenitez-bcn <dbr.bcn@gmail.com>
2021-06-03 00:13:07 +0200

    test logout logic

--- components/navigation/LogoutButton.spec.ts
+++ components/navigation/LogoutButton.spec.ts
@@ -1,9 +1,20 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import LogoutButton from "./LogoutButton.vue";
+import Vuetify from 'vuetify'
 
 describe('Logout button', () => {
-  test('is a Vue instance', () => {
-    const wrapper = shallowMount(LogoutButton)
-    expect(wrapper.vm).toBeTruthy()
+  const $auth = {
+    logout: jest.fn()
+  }
+  const sut = mount(LogoutButton, {
+    mocks: {
+      $auth
+    },
+    vuetify: new Vuetify()
+  })
+  it('should logout the user', () => {
+    sut.find('.v-list-item').trigger('click');
+
+    expect($auth.logout).toBeCalled();
   })
 })
