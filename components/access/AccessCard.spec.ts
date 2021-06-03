48d70aa30e71fa47cc79a82be1c56621b3783d8d
dbenitez-bcn <dbr.bcn@gmail.com>
2021-06-03 22:24:29 +0200

    adds set login to access card

--- components/access/AccessCard.spec.ts
+++ components/access/AccessCard.spec.ts
@@ -20,4 +20,11 @@ describe('Access card', () => {
         expect(wrapper.find(byDT("login-form")).exists()).toBeFalsy();
         expect(wrapper.find(byDT("register-form")).exists()).toBeTruthy();
     })
+    it('when clicking to login should show register form', async () => {
+        wrapper.find(byDT("login-tab")).trigger("click");
+        await wrapper.vm.$nextTick();
+        
+        expect(wrapper.find(byDT("login-form")).exists()).toBeTruthy();
+        expect(wrapper.find(byDT("register-form")).exists()).toBeFalsy();
+    })
 })
