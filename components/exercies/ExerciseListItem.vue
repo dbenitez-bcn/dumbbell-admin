cdacc03d8a105564908099659aa42918a0428e77
dbenitez-bcn <dbr.bcn@gmail.com>
2021-06-05 23:03:38 +0200

    create exercise list item

---
+++ components/exercies/ExerciseListItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-sheet
+    outlined
+    rounded
+    class="
+      exercise-card
+      clickable
+      d-flex
+      flex-row
+      align-center
+      justify-space-between
+      pa-2
+    "
+  >
+    <v-card-title class="pa-0">{{ exercise.name }}</v-card-title>
+    <div class="d-flex flex-row">
+      <EditBtn />
+      <div class="ml-2"></div>
+      <DeleteBtn @delete="deleteExercise" />
+    </div>
+  </v-sheet>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "nuxt-property-decorator";
+import { getModule } from "vuex-module-decorators";
+import ExerciseVM from "~/src/models/viewModels/ExerciseVM";
+import DeleteBtn from "../ui/buttons/DeleteBtn.vue";
+import EditBtn from "../ui/buttons/EditBtn.vue";
+import ExerciseStore from "~/store/exercise";
+
+@Component({
+  components: {
+    DeleteBtn,
+    EditBtn,
+  },
+})
+export default class ExercisesListItem extends Vue {
+  @Prop()
+  private readonly exercise!: ExerciseVM;
+
+  private async deleteExercise() {
+    if (window.confirm("Do you want to delete exercise?")) {
+      await getModule(ExerciseStore, this.$store).delete(this.exercise.id);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.exercise-card {
+  border-left: 8px var(--v-primary-base) solid;
+  transition: 0.1s;
+}
+.exercise-card:hover {
+  border-left: 16px var(--v-primary-base) solid;
+}
+</style>
