a0ee85cc488516d6928ec216e184dc00b31bbbb2
dbenitez-bcn <dbr.bcn@gmail.com>
2021-06-09 00:08:22 +0200

    fix constructor of null

--- nuxt.config.js
+++ nuxt.config.js
@@ -96,5 +96,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  render: {
+    bundleRenderer: {
+      runInNewContext: false,
+    },
+  },
 }
