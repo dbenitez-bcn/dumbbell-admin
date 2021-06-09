c87477220a3d03fc16aaa1854b79e78279e9dfdc
dbenitez-bcn <dbr.bcn@gmail.com>
2021-06-10 00:53:03 +0200

    adds edit page

---
+++ pages/exercises/_id/edit.vue
@@ -0,0 +1,16 @@
+<template>
+<h1>Edit exercise</h1>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "nuxt-property-decorator";
+
+@Component
+export default class ExerciseEditPage extends Vue {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
