60a725200b1b220133c026b54ecd59c4ec350daf
dbenitez-bcn <dbr.bcn@gmail.com>
2021-06-10 21:27:40 +0200

    adds exercise details

---
+++ components/exercies/ExerciseDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ exercise.name }}</h1>
+    <h1>{{ exercise.description }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "nuxt-property-decorator";
+import ExerciseVM from "~/src/models/viewModels/ExerciseVM";
+
+@Component
+export default class ExerciseDetails extends Vue {
+  @Prop()
+  exercise!: ExerciseVM;
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
