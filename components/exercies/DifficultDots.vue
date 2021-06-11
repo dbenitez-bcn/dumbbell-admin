349f1b4ff313bfa4210adbf6302d783235bc0227
dbenitez-bcn <dbr.bcn@gmail.com>
2021-06-11 23:21:28 +0200

    adds dots

---
+++ components/exercies/DifficultDots.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="row-dots">
+    <div
+      class="border-dot"
+      data-dt="full-dot"
+      v-for="index in getFullDots()"
+      :key="index*Math.random()"
+    >
+      <div class="full"></div>
+    </div>
+    <div
+      class="border-dot"
+      data-dt="empty-dot"
+      v-for="index in getEmptyDots()"
+      :key="index*Math.random()"
+    >
+      <div class="empty"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    value: Number
+  },
+  methods: {
+    getFullDots: function() {
+      return Math.round(+this.value / 2);
+    },
+    getEmptyDots: function() {
+      return 5 - this.getFullDots();
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+  $primary: var(--v-primary-base);
+
+.full {
+  width: 12px;
+  height: 12px;
+  background-color: $primary;
+  border-radius: 50%;
+}
+
+.empty {
+  width: 12px;
+  height: 12px;
+  background-color: transparent;
+}
+
+.border-dot {
+  margin: 0px 1px;
+  padding: 4px;
+  border: 1px solid $primary;
+  border-radius: 50%;
+}
+
+.row-dots {
+  display: flex;
+  flex-direction: row;
+  padding: 0px 4px;
+}
+</style>
--- components/exercies/ExerciseDetails.vue
+++ components/exercies/ExerciseDetails.vue
@@ -1,16 +1,28 @@
 <template>
-  <div>
-    <h1>{{ exercise.name }}</h1>
-    <h1>{{ exercise.description }}</h1>
-  </div>
+  <v-sheet outlined rounded class="pa-6">
+    <div class="row">
+      <h2 class="grey--text text--darken-1 mr-2"
+        >#{{ exercise.id }}</h2
+      >
+      <h2>{{ exercise.name }}</h2>
+    </div>
+<div class="row">
+  <DifficultDots :value="exercise.difficulty"/>
+</div>
+  </v-sheet>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Component, Prop } from "nuxt-property-decorator";
 import ExerciseVM from "~/src/models/viewModels/ExerciseVM";
+import DifficultDots from "./DifficultDots.vue";
 
-@Component
+@Component({
+  components: {
+    DifficultDots
+  }
+})
 export default class ExerciseDetails extends Vue {
   @Prop()
   exercise!: ExerciseVM;
