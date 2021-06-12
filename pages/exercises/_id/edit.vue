814164681e4621bdbf591d2fafaadff2cfd59d7f
dbenitez-bcn <dbr.bcn@gmail.com>
2021-06-12 11:41:16 +0200

    displays edit component

--- pages/exercises/_id/edit.vue
+++ pages/exercises/_id/edit.vue
@@ -1,14 +1,38 @@
 <template>
-<h1>Edit exercise</h1>
+  <Spinner v-if="$fetchState.pending" />
+  <KettlyError
+    v-else-if="$fetchState.error"
+    :message="'Kettly couldn\'t find the exercise'"
+  />
+  <div class="ma-6" v-else>
+    <ExerciseEditForm :exercise="exercise" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "nuxt-property-decorator";
+import { Component, getModule } from "nuxt-property-decorator";
+import ExerciseVM from "~/src/models/viewModels/ExerciseVM";
+import ExerciseStore from "~/store/exercise";
+import KettlyError from "~/components/ui/KettlyError.vue";
+import Spinner from "~/components/ui/Spinner.vue";
+import ExerciseEditForm from "~/components/exercies/ExerciseEditForm.vue";
 
-@Component
+@Component({
+  components: {
+    KettlyError,
+    Spinner,
+    ExerciseEditForm
+  },
+  fetchOnServer: false,
+})
 export default class ExerciseEditPage extends Vue {
-    
+  private exercise!: ExerciseVM;
+  async fetch() {
+    this.exercise = await getModule(ExerciseStore, this.$store).getById(
+      Number(this.$route.params.id)
+    );
+  }
 }
 </script>
 
